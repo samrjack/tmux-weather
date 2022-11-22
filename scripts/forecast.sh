9efc56e00495bd2db9e00f58a84e479bcbcf73a1
Samuel Jackson <samueljackson@bymason.com>
2022-11-22 10:48:59 -0800

    Add unit options and a safeguard
    Units can now be specified so that metric can be shown.
    
    If the connection to the server is slow or broken, it won't be
    printed on the tmux line since that can look very ugly.

--- scripts/forecast.sh
+++ scripts/forecast.sh
@@ -5,11 +5,21 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 print_forecast() {
+
+  # https://github.com/chubin/wttr.in#one-line-output
   local format=$(get_tmux_option @forecast-format "%C+%t+%w")
+
   local location=$(get_tmux_option @forecast-location "") # Let wttr.in figure out the location
+
   local char_limit=$(get_tmux_option @forecast-char-limit 75)
-  local forecast=$(curl http://wttr.in/$location?format=$format)
-  echo ${forecast:0:$char_limit}
+
+  # https://github.com/chubin/wttr.in#weather-units
+  local units=$(get_tmux_option @forecast-units "") # Let wttr.in figure out the units
+
+  local forecast=$(curl -G -d ${units} -d "format=${format}" http://wttr.in/${location})
+
+  # Only print a temp when successful so it doesn't clog up the line with an error
+  [ $? -eq 0 ] && echo ${forecast:0:$char_limit} || echo "??"
 }
 
 main() {
